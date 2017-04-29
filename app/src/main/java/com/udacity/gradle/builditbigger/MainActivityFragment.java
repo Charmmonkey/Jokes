9c7876ce7c5433b13584a29ca788755cd957ff94
Kevin Hsu <jeryen.hsu@gmail.com>
2017-04-28 19:17:49 -0700

    1.0.3 library dependencies and intents

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,13 +1,16 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.Jokes;
+import com.example.androidlibrary.JokesActivity;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
@@ -41,9 +44,21 @@ public class MainActivityFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
-
         Jokes jokes = new Jokes();
-        String randomJoke = jokes.tellJoke();
+        final String randomJoke = jokes.tellJoke();
+
+
+        Button button = (Button) getActivity().findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent jokesIntent = new Intent(getContext(), JokesActivity.class).putExtra("joke", randomJoke);
+                startActivity(jokesIntent);
+            }
+        });
+
+
         Toast.makeText(getContext(), randomJoke, Toast.LENGTH_SHORT).show();
     }
+
 }
